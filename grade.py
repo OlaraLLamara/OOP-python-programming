fc2351461634a2ba77bc94907617ddc3e0274a62
Olara L. Lamara <60262877+OlaraLLamara@users.noreply.github.com>
2022-10-10 13:14:23 +0300

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,18 +1,20 @@
-    
-    
-
-score = input("Please enter score between 0.1 and 1: ")
-
-if float(score) >= 0.9 and float(score)<=1:
-    print("Your Grade is A.")
-elif float(score) < 0.8 and float(score)<=1:
-    print("Your Grade is B")
-elif float(score) >= 0.7 and float(score)<=1:
-    print("Your Grade is C")
-elif float(score) >= 0.8 and float(score)<=1:
-        print("Your Grade is D")
-elif float(score) < 0.6 and float(score)<=1:
-    print("Your Grade is F")
-else: 
-    print("Bad Score")
-
+score=input("Please type a score between 0.0 and 1.0:")
+try:
+  float(score)
+  if float(score) >= 0.9 and float(score) <= 1.0:
+    print("A")
+  elif float(score) >= 0.8 and float(score) <= 0.9:
+    print("B")
+  elif float(score) >= 0.7 and float(score) <= 0.8:
+    print("C")
+  elif float(score) >= 0.6 and float(score) <= 0.7:
+    print("D")
+  elif float(score) > 0 and float(score) <= 0.6:
+    print("F")
+  elif score == 'stop':
+      exit()
+  else:
+    print("Bad score.  Please run the program again.")  
+except: 
+    print("program stopped by user.")
+    exit()
