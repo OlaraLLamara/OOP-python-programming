43bb77804e3945188b35df241d2772a912f19eb0
Olara L. Lamara <60262877+OlaraLLamara@users.noreply.github.com>
2022-10-16 11:42:10 +0300

    Update middle.py

--- middle.py
+++ middle.py
@@ -12,10 +12,10 @@ hat_list = [1, 2, 3, 4, 5]
 user_input_number = int(input("enter a number to replace the middle number in the array above"))
 
 
-hat_list[len(hat_list) // 2] = user_input_number
+hat_list[len(hat_list) // 2] = user_input_number #used to calculate the midle number and to asign user input to it
 print("the new list with middle number replaced is:", hat_list)
 
-hat_list.pop(-1)
+hat_list.pop(-1) #used to remove last element in list
 
 print("the list with the last number popped off is ", hat_list)
 
