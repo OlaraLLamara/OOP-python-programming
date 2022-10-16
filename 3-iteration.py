f7a1352af2c984cd5556d79017e87044ee7e7874
Olara L. Lamara <60262877+OlaraLLamara@users.noreply.github.com>
2022-10-16 11:08:34 +0300

    added email scanner

---
+++ 3-iteration.py
@@ -0,0 +1,16 @@
+for ch in "timothy.ddumba@guild.org": 
+    if ch == "@":
+        print (ch)
+        break
+    print(ch, end = "")
+    
+# to accept email through input.
+
+# email = str(input("enter email"))
+# for ch in email: 
+#     if ch == "@":
+#         print (ch)
+#         break
+#     print(ch, end = "")
+    
+    
--- Assignments 2/1-forloop.py
+++ Assignments 2/1-forloop.py
@@ -1,8 +1,7 @@
 #Create a for loop that counts from 0 to 10, and prints odd numbers to the screen.
 
-for i in range(0,10):
-   
-    if i%2 ==1:
+for i in range(0, 10):
+    if i % 2 != 0:
         print(i)
     
 
